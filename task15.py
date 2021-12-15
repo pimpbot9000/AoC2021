82ef36ba19823bf734184d64fe5dda1b202ba663
pimpbot9000 <t.valimaki@gmail.com>
2021-12-15 15:48:55 +0200

    Modify Dijkstra

--- task15.py
+++ task15.py
@@ -1,7 +1,6 @@
 import time
 from utils import load_data
 import numpy as np
-from queue import PriorityQueue
 import heapq
 
 INF = 10**10
@@ -30,8 +29,9 @@ class SeaFloor:
         for row in self.A[:, ]:
             print(''.join([str(x) for x in row]))
 
-    def get_adjacent_coordinates(self, i, j):
+    def get_adjacent_coordinates(self, coord):
         shape = self.A.shape
+        i, j = coord
         n = []
         if j - 1 >= 0:
             n.append((i, j - 1))
@@ -48,32 +48,67 @@ class SeaFloor:
         return n
 
     def dijkstra_search(self):
+        # A vanilla Dijkstra's algorithm to shortest paths.
+
         visited = np.zeros(self.A.shape, dtype=bool)
         distance = np.ones(self.A.shape, dtype=int) * INF
         distance[0, 0] = 0
-        previous = {}
+        seen = np.zeros(self.A.shape, dtype=bool)
+        pq = [(0, (0, 0))]
+
+        def get_next():
+            is_visited = True
+            current_distance, (i_current, j_current) = 0, (0, 0)
+            while is_visited:
+                current_distance, (i_current, j_current) = heapq.heappop(pq)
+                is_visited = visited[i_current, j_current]
+
+            return current_distance, (i_current, j_current)
+
+        while pq:
+
+            current_distance, (i_current, j_current) = get_next()
+            visited[i_current, j_current] = True
+
+            for i, j in self.get_adjacent_coordinates((i_current, j_current)):
+                if not visited[i, j] and not seen[i,j]:
+                    new_distance = current_distance + self.A[i, j]                    
+                    if new_distance < distance[i, j]:
+                       distance[i, j] = new_distance
+                       heapq.heappush(pq, (new_distance, (i, j)))
+        print(distance)
+
+    def dijkstra_search2(self):
+        # Modifier Dijkstra's algorithm to shortest paths.
+        # After the distance to a cell has been set from infinite to finite value
+        # due to the fact how "nodes" are connected and the order of the iteration,
+        # the distance is never relaxed. Using this fact one can implement a simplified
+        # version of the Dijkstra's algorithm.
+
+        visited = np.zeros(self.A.shape, dtype=bool)
+        distance = np.ones(self.A.shape, dtype=int) * INF
+        distance[(0, 0)] = 0
+        seen = np.zeros(self.A.shape, dtype=bool)
         pq = [(0, (0, 0))]
 
         while pq:
 
-            current_distance, (i_current, j_current) = heapq.heappop(pq)
-            visited[i_current, j_current] = True
+            current_distance, coord = heapq.heappop(pq)
+            visited[coord] = True
 
-            for i, j in self.get_adjacent_coordinates(i_current, j_current):
-                if not visited[i, j]:
-                    new_distance = current_distance + self.A[i, j]
-                    if new_distance < distance[i, j]:
-                        distance[i, j] = new_distance
-                        previous[(i, j)] = (i_current, j_current)
-                        heapq.heappush(pq, (new_distance, (i, j)))
+            for coord in self.get_adjacent_coordinates(coord):
+                if not visited[coord] and not seen[coord]:
+                    new_distance = current_distance + self.A[coord]  
+                    distance[coord] = new_distance
+                    heapq.heappush(pq, (new_distance, coord))
+                    seen[coord] = True
 
-        print(distance[self.A.shape[0]-1, self.A.shape[1]-1])
-
+        print(distance)
 
 data = [[int(x) for x in row] for row in load_data('data/data15.txt')]
 
 seafloor = SeaFloor(data, multiplier=5)
 start = time.time()
-seafloor.dijkstra_search()
+seafloor.dijkstra_search2()
 end = time.time()
 print("time", end-start, "s")
