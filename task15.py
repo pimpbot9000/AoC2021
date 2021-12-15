b1d035b67e9e74e78eeb5ce5828275c43a695e46
Tuomas Välimäki <t.valimaki@gmail.com>
2021-12-15 21:47:25 +0200

    Modify task 15 and add lessons to readme

--- task15.py
+++ task15.py
@@ -9,25 +9,51 @@ INF = 10**10
 class SeaFloor:
     def __init__(self, A, multiplier=1):
         A = np.array(A)
+        self.multiplier = multiplier
+        self.original = A
 
         if multiplier == 1:
-            self.A = A
+            self.A = A            
         else:
+            # build a large grid (for speed comparison)
             I = A.shape[0]
             J = A.shape[1]
-            self.A = np.zeros(
-                (A.shape[0] * multiplier, A.shape[1] * multiplier), dtype=int)
+
+            padded_list = []
             for i in range(multiplier):
                 for j in range(multiplier):
                     c = i + j
-                    padded = np.pad(
-                        A + c, [(I * i, I * (multiplier - 1 - i)), (J * j, J * (multiplier-1-j))])
+                    padded = np.pad(A + c, [(I * i, I * (multiplier - 1 - i)), (J * j, J * (multiplier-1-j))])
                     padded = np.where(padded > 9, padded % 9, padded)
-                    self.A = np.add(self.A, padded)
+                    padded_list.append(padded)
+
+            self.A = sum(padded_list)
+
+    def get_value(self, coord, use_virtual = True):
+        # getting the value from a virtual grid or from a
+        # created large grid (for speed comparison)
+
+        if not use_virtual:
+            return self.A[coord]       
+
+        if self.multiplier == 1:
+            return self.original[coord]
+        else:
+            i, j = coord
+            I, J = self.original.shape
+            c = i // I + j // J
+            value = self.original[i % I, j % J] + c
+            return value if value <= 9 else value % 9
+
+    def get_shape(self):
+        return self.multiplier * self.original.shape[0], self.multiplier * self.original.shape[1]
 
     def print(self):
         for row in self.A[:, ]:
             print(''.join([str(x) for x in row]))
+    def print_virtual(self):
+        for i in range(self.get_shape()[0]):
+            print(''.join([str(self.get_value((i,j))) for j in range(self.get_shape()[1])]))
 
     def get_adjacent_coordinates(self, coord):
         shape = self.A.shape
@@ -47,13 +73,16 @@ class SeaFloor:
 
         return n
 
-    def dijkstra_search(self):
-        # A vanilla Dijkstra's algorithm to shortest paths.
+    def dijkstra_search(self, use_virtual=True):
+        # A vanilla Dijkstras algorithm for shortest paths.
+        # There exists redundancy considering the problem 
+        # (See method dijkstra_search2)
+        #
+        # Does not keep track of the shortest paths.
 
-        visited = np.zeros(self.A.shape, dtype=bool)
-        distance = np.ones(self.A.shape, dtype=int) * INF
+        visited = np.zeros(self.get_shape(), dtype=bool)
+        distance = np.ones(self.get_shape(), dtype=int) * INF
         distance[0, 0] = 0
-        seen = np.zeros(self.A.shape, dtype=bool)
         pq = [(0, (0, 0))]
 
         def get_next():
@@ -71,24 +100,28 @@ class SeaFloor:
             visited[i_current, j_current] = True
 
             for i, j in self.get_adjacent_coordinates((i_current, j_current)):
-                if not visited[i, j] and not seen[i,j]:
-                    new_distance = current_distance + self.A[i, j]                    
+                if not visited[i, j]:
+                    new_distance = current_distance + self.get_value((i, j), use_virtual=use_virtual)                    
                     if new_distance < distance[i, j]:
                        distance[i, j] = new_distance
                        heapq.heappush(pq, (new_distance, (i, j)))
-        print(distance)
 
-    def dijkstra_search2(self):
-        # Modifier Dijkstra's algorithm to shortest paths.
+        print(distance)
+    
+    def dijkstra_search2(self, use_virtual=True):
+        # Simplified Dijkstras algorithm for shortest paths.
         # After the distance to a cell has been set from infinite to finite value
         # due to the fact how "nodes" are connected and the order of the iteration,
-        # the distance is never relaxed. Using this fact one can implement a simplified
-        # version of the Dijkstra's algorithm.
+        # the distance is never relaxed twice. Using this fact one can implement 
+        # a simplified version of the Dijkstra's algorithm where relaxation is done 
+        # only once for each node.
+        #
+        # Does not keep track of the shortest paths.
 
-        visited = np.zeros(self.A.shape, dtype=bool)
-        distance = np.ones(self.A.shape, dtype=int) * INF
+        visited = np.zeros(self.get_shape(), dtype=bool)
+        distance = -1 * np.ones(self.get_shape(), dtype=int) 
         distance[(0, 0)] = 0
-        seen = np.zeros(self.A.shape, dtype=bool)
+        relaxed = np.zeros(self.get_shape(), dtype=bool)
         pq = [(0, (0, 0))]
 
         while pq:
@@ -97,18 +130,19 @@ class SeaFloor:
             visited[coord] = True
 
             for coord in self.get_adjacent_coordinates(coord):
-                if not visited[coord] and not seen[coord]:
-                    new_distance = current_distance + self.A[coord]  
+                if not relaxed[coord] and not visited[coord]:
+                    new_distance = current_distance + self.get_value(coord, use_virtual=use_virtual)
                     distance[coord] = new_distance
                     heapq.heappush(pq, (new_distance, coord))
-                    seen[coord] = True
+                    relaxed[coord] = True
 
         print(distance)
 
 data = [[int(x) for x in row] for row in load_data('data/data15.txt')]
 
 seafloor = SeaFloor(data, multiplier=5)
+print("grid build finished")
 start = time.time()
-seafloor.dijkstra_search2()
+seafloor.dijkstra_search(use_virtual=False)
 end = time.time()
 print("time", end-start, "s")
