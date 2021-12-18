76f1fc1005d2a3e2421e1d465e2aa1485977df15
pimpbot9000 <t.valimaki@gmail.com>
2021-12-18 19:09:53 +0200

    Add 'nonlocal' variables

--- task18.py
+++ task18.py
@@ -3,107 +3,113 @@ import math
 class Number:
     def __init__(self, number):
         self.number = number
-        self.exploded = False
-        self.splitted = False
-        self.explodable = False
-        self.splittable = False
+
+
 
     def add(self, other):
         return Number([self.number, other.number])
         
     def print(self):
-        print("Numbver", self.number)
+        print("Number", self.number)
 
     def explode2(self):
 
-        self.exploded = False
+        exploded = False
 
         def recurse(n, depth):
-
-            if depth >= 5 and type(n) == list and type(n[0]) == int and type(n[1]) == int and not self.exploded:
-                self.exploded = True
+            nonlocal exploded
+            if depth >= 5 and type(n) == list and type(n[0]) == int and type(n[1]) == int and not exploded:
+                exploded = True
                 
-                return True, n[0], n[1], 0           
+                return n[0], n[1], 0           
                 
             elif type(n) == int:
-                return False, 0,0, n
+                return 0,0, n
 
             else:
-                ex_left, cl_left, cr_left, left = recurse(n[0], depth + 1 )
-                ex_right, cl_right, cr_right, right = recurse(n[1], depth + 1 )
+                # cl = "carry left"
+                # ct = "carry right"
+                cl_left, cr_left, left = recurse(n[0], depth + 1 )
+                cl_right, cr_right, right = recurse(n[1], depth + 1 )
 
                 if cl_left != 0 or cr_left != 0:
 
                     if type(right) == int:
-                        return True, cl_left, 0, [left, right + cr_left]
+                        return cl_left, 0, [left, right + cr_left]
                     elif type(right) == list:
-                        suc, res = push_right(right, cr_left)
-                        return True, cl_left, 0, [left,res]
+                        new_right = push_right(right, cr_left)
+                        return cl_left, 0, [left, new_right]
                     else:
-                        return True, cl_left, cr_right, [left, right]
+                        return cl_left, cr_right, [left, right]
 
                 elif cl_right != 0 or cr_right != 0:    
 
                     if type(left) == int:                        
-                        return True, 0, cr_right, [left + cl_right, right]
+                        return 0, cr_right, [left + cl_right, right]
                     elif type(left) == list:
-                        success, result = push_left(left, cl_right)
-                        return True, 0, cr_right, [result, right]
+                        new_left = push_left(left, cl_right)
+                        return 0, cr_right, [new_left, right]
 
                 else:
-                    return False, 0,0, [left, right]
+                    return 0,0, [left, right]
 
         def push_right(number, push): # push to right side leftmost
 
             if type(number) == int:
-                return True, number + push
+                return number + push
             else:
-                push1, left = push_right(number[0], push)
-                push2, right = push_right(number[1], 0)
-                return push1 or push2, [left, right]
+                left = push_right(number[0], push)
+                right = push_right(number[1], 0)
+                return [left, right]
 
         def push_left(number, push): # push to left side rightmost
             if type(number) == int:
-                return True, number + push
+                return number + push
             else:
-                push1, left = push_left(number[0], 0)
-                push2, right = push_left(number[1], push)
-                return push1 or push2, [left, right]
+                left = push_left(number[0], 0)
+                right = push_left(number[1], push)
+                return [left, right]
 
-        a, b, c, d = recurse(self.number,1)
-        self.number = d
+        _, _, result = recurse(self.number,1)
 
-        return self.exploded
+        self.number = result
+
+        return exploded
         
     def is_explodable(self):
         
-        self.explodable = False
+        explodable = False
 
         def recurse(n, depth):
+            nonlocal explodable
             if depth >= 5:
-                self.explodable = True
+                explodable = True
             else:
-                if type(n[0]) == list and not self.explodable:
+                if type(n[0]) == list and not explodable:
                     recurse(n[0], depth + 1 )
-                if type(n[1]) == list and not self.explodable:
+                if type(n[1]) == list and not explodable:
                     recurse(n[1], depth + 1)
 
         recurse(self.number, 1)
-        return self.explodable
+        return explodable
     
     def is_splittable(self):
-        self.splittable = False
+
+        splittable = False
+
         def recurse(n, depth):
-            if type(n) == int and n >= 10:
-                self.splittable = True
+            nonlocal splittable
+            if type(n) == int and n >= 10:                
+                splittable = True
             else:
-                if not self.splittable and type(n) == list:
+                if not splittable and type(n) == list:
                     recurse(n[0], depth + 1)
-                if not self.splittable and type(n) == list:
+                if not splittable and type(n) == list:
                     recurse(n[1], depth +1)
 
         recurse(self.number, 1)
-        return self.splittable
+
+        return splittable
         
     def reduce2(self):
 
@@ -116,19 +122,20 @@ class Number:
                 break
 
     def split(self):
-        self.splitted = False
+        splitted = False
         def recurse(n):
-            if type(n) == int and n >= 10 and not self.splitted:
-                self.splitted = True
-                return True, [int(math.floor(n / 2)), int(math.ceil(n / 2))]
+            nonlocal splitted
+            if type(n) == int and n >= 10 and not splitted:
+                splitted = True
+                return [int(math.floor(n / 2)), int(math.ceil(n / 2))]
             elif type(n) == int:
-                return False, n
+                return n
             else:
-                split_left, left = recurse(n[0])
-                split_right, right = recurse(n[1])
-                return split_left or split_right, [left, right]
+                left = recurse(n[0])
+                right = recurse(n[1])
+                return [left, right]
         
-        splitted, self.number = recurse(self.number)
+        self.number = recurse(self.number)
 
         return splitted
         
@@ -152,19 +159,16 @@ for row in data:
    numbers.append(eval(row))
 
 def part1():
-
     summa = Number(numbers[0])
     for i in range(1, len(numbers)):
         summa = summa.add(Number(numbers[i]))
         summa.reduce2()
-
 
     print(summa.magnitude())
 
 
 def part2():
     max_magnitude = 0
-    #numbers = [1,2,3,4,5,6]
 
     for i in range(0, len(numbers) - 1):
         for j in range(i + 1, len(numbers)):
@@ -182,4 +186,6 @@ def part2():
             max_magnitude = max(max_magnitude, magnitude2)
 
     print(max_magnitude)
+
+part1()
 part2()
