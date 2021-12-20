b8fb919064087779f9072829479c60254f7de9bf
Tuomas Välimäki <t.valimaki@gmail.com>
2021-12-20 22:22:11 +0200

    Optimize task 19

--- task19.py
+++ task19.py
@@ -27,13 +27,13 @@ rots = [lambda x, y, z: (x, y, z),
         ]
 
 def parse_data():
-    data = load_data("data/data19.test.txt")
+    data = load_data("data/data19.txt")
     scanners = []
     coordinates = []
 
     for row in data:
         if row == "" or row == "end":
-            coordinates.append((0, 0, 0, 1))
+            coordinates.append((0, 0, 0, 1)) # append scanners to probe list
             scanners.append(coordinates)
             coordinates = []
         elif row[0:2] == "--":
@@ -67,33 +67,50 @@ def match(scanner1, scanner2):
     return max_union
 
 
-unions = []
-# match scanners pairwise
-for i in range(0, len(scanners) - 1):
-    for j in range(i + 1, len(scanners)):
-        print("match", i, j)
-        scanner1 = scanners[i]
-        scanner2 = scanners[j]
-        for s in scanner1:
-            scanner1_zeroed = zero(s, scanner1)
-            # scanner 2 coordinates are permutated.
-            m = match(scanner1_zeroed, scanner2)
-            if len(m) > 0:
-                unions.append((set((i, j)), m))
-                break
+def manhattan(x, y):
+    return abs(x[0] - y[0]) + abs(x[1] - y[1]) + abs(x[2] - y[2])
 
+def calculate_distances(coords):
+    coords = list(filter(lambda x: len(x) == 3, coords))
+    distances = set() 
+    for i in range(0, len(coords) - 1):
+        for j in range(i + 1, len(coords)):
+            distances.add(manhattan(coords[i], coords[j]))
+    return distances
 
-def match2(union1_input, union2_input):
-    scanners1, union1 = union1_input
-    scanners2, union2 = union2_input
+def match_pairwise(scanners):
+    unions = []
+    # match scanners pairwise
+    for i in range(0, len(scanners) - 1):
+        for j in range(i + 1, len(scanners)):
+            print("match", i, j)
+            scanner1 = scanners[i]
+            scanner2 = scanners[j]
+            # Check if contains same distances
+            # If not, does not overlap
+            distances1 = calculate_distances(scanner1)
+            distances2 = calculate_distances(scanner2)
+            if len(distances1.intersection(distances2)) < 70:
+                continue
+            for s in scanner1:
+                scanner1_zeroed = zero(s, scanner1)
+                # scanner 2 coordinates are permutated.
+                m = match(scanner1_zeroed, scanner2)
+                if len(m) > 0:
+                    unions.append((set((i, j)), m))
+                    break
+    return unions
+
+def match2(union1, union2):
 
     for point in union1:
         scanner1_zero = zero(point, union1)
         # scanner 2 coordinates are permutated.
         m = match(scanner1_zero, union2)
         if len(m) > 0:
-            return (scanners1.union(scanners2), m)
+            return m
 
+unions = match_pairwise(scanners)
 
 used = set((0,))
 total_union = unions[0][1]
@@ -103,22 +120,17 @@ while len(contains) != len(scanners):
     for i in range(len(unions)):
         if i not in used:
             if len(contains.intersection(unions[i][0])) > 0:
-                print("match", contains.intersection(unions[i][0]))
+                print("Union:", contains.union(unions[i][0]))
                 used.add(i)
-                u, m = match2((contains, total_union), unions[i])
-                contains = u
-                total_union = m
-                print(contains)
+                total_union = match2(total_union, unions[i][1])
+                contains = contains.union(unions[i][0])
+
+                
 print("result")
 print(len(total_union) - len(scanners))
 
 probes = list(filter(lambda x: len(x) == 4, total_union))
 print(probes)
-
-
-def manhattan(x, y):
-    return abs(x[0] - y[0]) + abs(x[1] - y[1]) + abs(x[2] - y[2])
-
 
 max_manhattan = 0
 for i in range(0, len(probes) - 1):
--- task20.py
+++ task20.py
@@ -5,25 +5,24 @@ def load_and_parse():
     # Loads and maps # -> 1 and . -> 0
     data = load_data("data/data20.txt")
     algorithm = [1 if x == "#" else 0 for x in data[0]]
-    
+
     rows = []
     for i in range(2, len(data)):
         r = [1 if x == "#" else 0 for x in data[i]]
         rows.append(r)
     return algorithm, rows
-    
+
+
 algorithm, rows = load_and_parse()
+
 
 class Image:
     def __init__(self, algorithm, rows):
         self.algorithm = algorithm
         self.points = set()
         self.padding = 2
-
-        for i in range(len(rows)):
-            for j in range(len(rows[0])):
-                if rows[i][j] == 1:
-                    self.points.add((i, j))
+        self.points = {(i, j) for i in range(len(rows))
+                       for j in range(len(rows[0])) if rows[i][j] == 1}
 
         self.min_i = self.get_min_i()
         self.max_i = self.get_max_i()
@@ -33,9 +32,8 @@ class Image:
         self.rim = False
 
     def get_value(self, coord, p=0):
-        # gets coordinate values
-        i, j = coord        
-        if self.rim and (i < self.min_i + p or i > self.max_i - p or j < self.min_j + p   or j > self.max_j - p):            
+        i, j = coord
+        if self.rim and (i < self.min_i + p or i > self.max_i - p or j < self.min_j + p or j > self.max_j - p):
             return 1
         else:
             return 1 if coord in self.points else 0
@@ -55,7 +53,7 @@ class Image:
     def get_grid_value(self, coord, p=0):
         # get binary value for 3x3 grid
         bin_str = ''.join([str(self.get_value((coord[0] + i, coord[1] + j), p))
-                          for i in range(-1, 2) for j in range(-1, 2)])        
+                          for i in range(-1, 2) for j in range(-1, 2)])
         return int(bin_str, 2)
 
     def get_algorithm_value(self, number):
@@ -72,32 +70,32 @@ class Image:
         enhanced = set()
 
         counter = 0
-        
+
         self.min_i -= self.padding
         self.max_i += self.padding
         self.min_j -= self.padding
         self.max_j += self.padding
 
-        for i in range(self.min_i, self.max_i + 1 ):
+        for i in range(self.min_i, self.max_i + 1):
             for j in range(self.min_j, self.max_j + 1):
                 grid_value = self.get_grid_value((i, j), self.padding)
                 pixel_value = self.get_algorithm_value(grid_value)
                 if pixel_value == 1:
-                    enhanced.add((i,j))
-                if i == self.min_i or i == self.max_i  or j == self.min_j  or j == self.max_j:
+                    enhanced.add((i, j))
+                if i == self.min_i or i == self.max_i or j == self.min_j or j == self.max_j:
                     if pixel_value == 1:
                         counter += 1
-        
+
         height = abs(self.max_i - self.min_i + 1)
         width = abs(self.max_j - self.min_j + 1)
-        
 
-        if counter ==  2 * height + 2 * width - 4:
+        if counter == 2 * height + 2 * width - 4:
             self.rim = True
             self.padding = 1
         else:
             self.rim = False
         self.points = enhanced
+
 
 image = Image(algorithm, rows)
 
@@ -106,7 +104,4 @@ for i in range(50):
     print("round", i)
 
 image.print()
-print(len(im.points))
-
-
-
+print(len(image.points))
